62ff3c1a6ef38cfb166351280f780a2142524bda
romquentin <rom.quentin@gmail.com>
2023-09-25 14:46:35 +0200

    Add decod_stats and gat_stats

--- base.py
+++ base.py
@@ -45,3 +45,42 @@ def create_seq(n_trials, structure):
                     else:
                         data.sound.at[trial] = sounds[3]
     return data
+
+
+def decod_stats(X):
+    from mne.stats import permutation_cluster_1samp_test
+    """Statistical test applied across subjects"""
+    # check input
+    X = np.array(X)
+
+    # stats function report p_value for each cluster
+    T_obs_, clusters, p_values, _ = permutation_cluster_1samp_test(
+        X, out_type='mask', n_permutations=2**12, n_jobs=6,
+        verbose=False)
+
+    # format p_values to get same dimensionality as X
+    p_values_ = np.ones_like(X[0]).T
+    for cluster, pval in zip(clusters, p_values):
+        p_values_[cluster] = pval
+
+    return np.squeeze(p_values_)
+
+
+def gat_stats(X):
+    from mne.stats import spatio_temporal_cluster_1samp_test
+    """Statistical test applied across subjects"""
+    # check input
+    X = np.array(X)
+    X = X[:, :, None] if X.ndim == 2 else X
+
+    # stats function report p_value for each cluster
+    T_obs_, clusters, p_values, _ = spatio_temporal_cluster_1samp_test(
+        X, out_type='mask',
+        n_permutations=2**12, n_jobs=-1, verbose=False)
+
+    # format p_values to get same dimensionality as X
+    p_values_ = np.ones_like(X[0]).T
+    for cluster, pval in zip(clusters, p_values):
+        p_values_[cluster.T] = pval
+
+    return np.squeeze(p_values_).T
